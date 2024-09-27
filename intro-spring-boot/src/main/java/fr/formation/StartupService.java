a3b230c8238447b9afcbaab5de9971b764da18e4
Jeremy <jeremy.perrouault@ascadis.fr>
2024-09-27 14:36:18 +0200

    JOUR 1 - INJECTION PAR CTOR

--- intro-spring-boot/src/main/java/fr/formation/StartupService.java
+++ intro-spring-boot/src/main/java/fr/formation/StartupService.java
@@ -1,15 +1,20 @@
 package fr.formation;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import fr.formation.musicien.Guitariste;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class StartupService implements CommandLineRunner {
-    @Autowired
-    private Guitariste guitariste;
+    // @Autowired
+    private final Guitariste guitariste;
+
+    // public StartupService(Guitariste guitariste) {
+    //     this.guitariste = guitariste;
+    // }
 
     @Override
     public void run(String... args) throws Exception {
