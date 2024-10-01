37214c836a98e62684eec4449d93c1066b507476
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-01 12:30:55 +0200

    JOUR 3 - SPRING SECURITY - FILTER

--- formation-security/src/main/java/fr/formation/config/JwtHeaderFilter.java
+++ formation-security/src/main/java/fr/formation/config/JwtHeaderFilter.java
@@ -1,10 +1,12 @@
 package fr.formation.config;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -19,24 +21,37 @@ import jakarta.servlet.http.HttpServletResponse;
 public class JwtHeaderFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String authHeader = request.getHeader("Authorization");
+        String token = this.getToken(request);
 
-        // Si pas de authHeader => Pas d'authentification
-        // Si authHeader == Bearer admin => Authentifier en tant qu'admin
-        // Si authHeader == Bearer ??? => Authentifier en tant qu'utilisateur
+        if (token != null) {
+            List<GrantedAuthority> authorities = new ArrayList<>();
 
-        Authentication authentication = new UsernamePasswordAuthenticationToken(
-            "username",
-            "username",
-            List.of(
-                new SimpleGrantedAuthority("ROLE_ADMIN")
-            )
-        );
+            if (token.equals("admin")) {
+                authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+            }
 
-        // On ajoute l'authentification au contexte de Sécurité de Spring Security
-        SecurityContextHolder.getContext().setAuthentication(authentication);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "username",
+                "username",
+                authorities
+            );
+
+            // On ajoute l'authentification au contexte de Sécurité de Spring Security
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
 
         // Passer à la suite de la chaine de filtres
         filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || authHeader.length() <= 7) {
+            return null;
+        }
+
+        // Bearer letoken
+        return authHeader.substring(7);
     }
 }
