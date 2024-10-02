ae636d6994d06a12a5d6f70da87923c1a186f570
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-02 09:43:59 +0200

    JOUR 4 - INIT TEST JPA

--- formation-test/src/test/java/fr/formation/FormationJpaApplicationTests.java
+++ formation-test/src/test/java/fr/formation/FormationJpaApplicationTests.java
@@ -1,13 +1,43 @@
 package fr.formation;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Optional;
 
-@SpringBootTest
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import fr.formation.model.Produit;
+import fr.formation.repo.ProduitRepository;
+
+// @SpringBootTest
+@DataJpaTest
 class FormationJpaApplicationTests {
+
+	@Autowired
+	private ProduitRepository produitRepository;
+
+	@Test
+	void shouldFindById() {
+		Optional<Produit> optProduit = this.produitRepository.findById("produitId");
+	}
 
 	@Test
 	void contextLoads() {
-	}
+		Assertions.assertEquals(true, true);
+		Assertions.assertEquals(10, 6 + 4);
+		Assertions.assertTrue(true);
+		Assertions.assertTrue((6 + 4 == 10));
+		Assertions.assertFalse(false);
+		Assertions.assertNull(null);
+		Assertions.assertNotNull("null");
 
+		Assertions.assertThrows(
+			RuntimeException.class
+			,
+			() -> {
+				throw new RuntimeException();
+			}
+		);
+	}
 }
---
+++ formation-test/src/test/resources/data.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO produit (pro_id, pro_name, pro_price, pro_date)
+VALUES ('produitId', 'Produit Nom', 8526, '2024-10-02 11:00:53');
