318492708e74a262f03dd058c651c7acba456280
Jeremy <jeremy.perrouault@ascadis.fr>
2024-10-02 11:15:05 +0200

    JOUR 4 - TEST MOCK MVC

--- formation-test/src/test/java/fr/formation/api/ProduitApiControllerTest.java
+++ formation-test/src/test/java/fr/formation/api/ProduitApiControllerTest.java
@@ -3,12 +3,17 @@ package fr.formation.api;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import fr.formation.model.Produit;
 import fr.formation.repo.ProduitRepository;
@@ -21,6 +26,33 @@ public class ProduitApiControllerTest {
 
     @InjectMocks
     private ProduitApiController ctrl;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders
+            .standaloneSetup(this.ctrl)
+            .build()
+        ;
+    }
+
+    @Test
+    void shouldFindAllStatusOk() throws Exception {
+        // given
+
+        // when
+        this.mockMvc
+            .perform(
+                MockMvcRequestBuilders.get("/api/produit")
+            )
+        
+        // then
+            .andExpect(MockMvcResultMatchers.status().isOk())
+        ;
+
+        Mockito.verify(this.repository).findAll();
+    }
 
     @Test
     void shouldFindAllReturnsProduitResponses() {
