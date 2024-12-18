07e2e788a1331d618c149d51ff4c7ff1eae39033
jperrouault-school-job <jeremy.perrouault@gmail.com>
2024-12-18 09:25:13 +0000

    JOUR 1 - SINGLETON

--- DP/singleton/SingletonApplication.java
+++ DP/singleton/SingletonApplication.java
@@ -1,10 +1,23 @@
 package singleton;
 
-public class SingletonApplication {
-    public static void main(String[] args) {
-        Singleton s1 = new Singleton();
-        Singleton s2 = new Singleton();
+import java.lang.reflect.Constructor;
 
+public class SingletonApplication {
+    public static void main(String[] args) throws Exception {
+        // Singleton s1 = new Singleton();
+        // Singleton s2 = new Singleton();
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        
         System.out.println(s1 == s2);
+
+        Constructor<Singleton> ctor = Singleton.class.getDeclaredConstructor();
+
+        ctor.setAccessible(true);
+
+        Singleton s3 = ctor.newInstance();
+        Singleton s4 = ctor.newInstance();
+
+        System.out.println(s3 == s4);
     }
 }
